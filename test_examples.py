0a3122fe896bd9f058e396921f1bcc71e65dd32c
mikhail.osinovskiy <mihaosin@yandex.ru>
2023-09-21 14:35:00 +0300

    3.01_Pytest_simple_tests

---
+++ test_examples.py
@@ -0,0 +1,12 @@
+class TestExamples:
+    def test_check_math(selfself):
+        a = 5
+        b = 9
+        expecped_sum = 14
+        assert a + b == expepred_sum, f"Sum of variables a and b is not equal to {expepred_sum}"
+
+    def test_check_math2(selfself):
+        a = 5
+        b = 11
+        expepred_sum = 14
+        assert a+b == expepred_sum, f"Sum of variables a and b is not equal to {expepred_sum}"
