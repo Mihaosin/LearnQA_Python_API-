222104059c4a17ea374f78140cf3ba89a7cc4706
mikhail.osinovskiy <mihaosin@yandex.ru>
2023-10-05 12:03:54 +0300

    Ex15: Тесты на метод user vol.2

--- Ex15_test_user_register.py
+++ Ex15_test_user_register.py
@@ -60,52 +60,52 @@ class TestUserRegisration(BaseCase):
         self.email = f"{base_part}{random_part}@{domain}"
 
     # - Создание пользователя с некорректным email - без символа @
-    # def test_create_user_with_wrong_email(self):
-    #     email = 'mihail&example.com'
-    #     data = {
-    #         'password': '123',
-    #         'username': 'learnqa',
-    #         'firstName': 'learnqa',
-    #         'lastName': 'learnqa',
-    #         'email': email
-    #     }
-    #
-    #     response = MyRequests.post("/user/", data=data)
-    #
-    #     Assertions.assert_code_status(response, 400)
-    #     assert response.content.decode("utf-8") == "Invalid email format"
-    #     print(response.content)
+    def test_create_user_with_wrong_email(self):
+        email = 'mihail&example.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format"
+        print(response.content)
 
     # - Создание пользователя без указания одного из полей - с помощью @parametrize необходимо проверить,
     # что отсутствие любого параметра не дает зарегистрировать пользователя
-    # @pytest.mark.parametrize('input', exclude_params)
-    # def test_create_user_without_required_param(self, input):
-    #     name = input["name"]
-    #     data = input["data"]
-    #
-    #     response = MyRequests.post("/user/", data=data)
-    #     actual_answer = response.text[0:40]
-    #     required_answer = "The following required params are missed"
-    #     print(name)
-    #     print(response.text)
-    #     assert actual_answer == required_answer, "Нет диагностики об отсутствующем параметре"
+    @pytest.mark.parametrize('input', exclude_params)
+    def test_create_user_without_required_param(self, input):
+        name = input["name"]
+        data = input["data"]
+
+        response = MyRequests.post("/user/", data=data)
+        actual_answer = response.text[0:40]
+        required_answer = "The following required params are missed"
+        print(name)
+        print(response.text)
+        assert actual_answer == required_answer, "Нет диагностики об отсутствующем параметре"
 
     # - Создание пользователя с очень коротким именем в один символ
-    # def test_create_user_with_short_name(self):
-    #     email = 'mihail@example.com'
-    #     data = {
-    #         'password': '123',
-    #         'username': 'learnqa',
-    #         'firstName': '1',
-    #         'lastName': 'learnqa',
-    #         'email': email
-    #     }
-    #
-    #     response = MyRequests.post("/user/", data=data)
-    #
-    #     Assertions.assert_code_status(response, 400)
-    #     assert response.content.decode("utf-8") == "The value of 'firstName' field is too short"
-    #     print(response.content)
+    def test_create_user_with_short_name(self):
+        email = 'mihail@example.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': '1',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'firstName' field is too short"
+        print(response.content)
 
     # - Создание пользователя с очень длинным именем - длиннее 250 символов
     def test_create_user_with_long_name(self):
