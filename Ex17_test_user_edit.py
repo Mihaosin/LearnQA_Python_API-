43a160b5a7d3dd727efaaea79c80c88caa19eb69
mikhail.osinovskiy <mihaosin@yandex.ru>
2023-10-05 14:29:34 +0300

    Ex17: Негативные тесты на PUT

---
+++ Ex17_test_user_edit.py
@@ -0,0 +1,84 @@
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+
+class TestUserEdit(BaseCase):
+    # def test_edit_by_not_auth_user(self):
+    #     # - Попытаемся изменить данные пользователя, будучи неавторизованными
+    #     new_name = "Changed_name"
+    #
+    #     response1 = MyRequests.put(
+    #          f"/user/2",
+    #          )
+    #
+    #     Assertions.assert_code_status(response1, 400)
+    #     print(response1.content)
+
+    # def test_edit_another_user(self):
+    #     # - Попытаемся изменить данные пользователя, будучи авторизованными другим пользователем
+    #     data = {
+    #         'email': 'vinkotov@example.com',
+    #         'password': '1234'
+    #     }
+    #
+    #     response2 = MyRequests.post("/user/login", data=data)
+    #
+    #     auth_sid = self.get_cookie(response2, "auth_sid")
+    #     token = self.get_header(response2, "x-csrf-token")
+    #     user_id_from_auth_method = self.get_json_value(response2, "user_id")
+    #
+    #     # EDIT
+    #     new_name = "Changed_name"
+    #
+    #     response3 = MyRequests.put(
+    #          f"/user/80956",
+    #          headers={"x-csrf-token": token},
+    #          cookies={"auth_sid": auth_sid},
+    #          data={"firstName": new_name}
+    #      )
+    #
+    #     Assertions.assert_code_status(response3, 400)
+    #     print(response3.content)
+
+    def test_edit_another_user(self):
+        # - Попытаемся изменить firstName пользователя,
+        # будучи авторизованными тем же пользователем,
+        # на очень короткое значение в один символ
+
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        Assertions.assert_code_status(response2, 200)
+
+        # EDIT
+        new_name = "1"
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+        print(response3.content)
